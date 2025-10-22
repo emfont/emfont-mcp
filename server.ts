48351b3c8ad510d7dcf6296b1ac8d79a9ec19566
Elvis Mao <info@elvismao.com>
2025-10-22 13:36:03 +0800

    fix: port

--- server.ts
+++ server.ts
@@ -26,10 +26,11 @@ app.register(streamableHttp, {
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 app.listen({
-    port,
+    port: port,
+    host: "0.0.0.0",
 })
     .then(() => {
-        console.log(`Server is running on localhost:${port}/mcp`);
+        console.log(`Server is running on http://0.0.0.0:${port}/mcp`);
     })
     .catch(err => {
         console.error(err);
